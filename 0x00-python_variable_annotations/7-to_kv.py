1b96b2b13e44bd39aac371961ac95fb74e901dba
Scion-Kin <dinturner17@gmail.com>
2024-06-06 13:22:55 +0200

    Task 7 done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 ''' This module defines a function '''
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    ''' Returns a tuple '''
+
+    return (k, v ** 2)
