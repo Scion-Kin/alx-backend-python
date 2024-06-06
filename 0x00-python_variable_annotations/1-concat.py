2ea5e010ed26dc4721eae5cbd6ec2fe0f822f7a4
Scion-Kin <dinturner17@gmail.com>
2024-06-06 12:24:56 +0200

    Task 1 done

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 ''' This module defines a function '''
 
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     ''' Concatenates 2 strings and returns the resulting string '''
 
     return str1 + str2
