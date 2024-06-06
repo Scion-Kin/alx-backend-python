b46978941710c1ef2c5b97e7583064f7920b819d
Scion-Kin <dinturner17@gmail.com>
2024-06-06 12:23:14 +0200

    Task 1 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' This module defines a function '''
+
+
+def concat(str1: str, str2: str):
+    ''' Concatenates 2 strings and returns the resulting string '''
+
+    return str1 + str2
