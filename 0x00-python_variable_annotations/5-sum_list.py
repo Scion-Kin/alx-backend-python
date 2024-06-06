5c696e297694f4f58c4aec27874f9df0b593ea95
Scion-Kin <dinturner17@gmail.com>
2024-06-06 12:46:57 +0200

    Task 5 done

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 ''' This module defines a function '''
+
+from typing import List
+import math
+
+
+def sum_list(input_list: List[float]) -> float:
+    ''' Returns a sum of floating point numbers from a list '''
+
+    return math.fsum(input_list)
