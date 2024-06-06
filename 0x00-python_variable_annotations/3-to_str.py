cd9e8eef203e44152e14b2c9732a6059f058f226
Scion-Kin <dinturner17@gmail.com>
2024-06-06 12:35:15 +0200

    Task 3 done

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 ''' This module defines a function '''
+
+
+def to_str(n: float) -> str:
+    ''' Returns the string representation of a floating point number '''
+
+    return str(n)
