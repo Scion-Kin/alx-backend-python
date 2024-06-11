f30032e0d7b158897b01cb3d16068c994723ddb3
Scion-Kin <dinturner17@gmail.com>
2024-06-11 12:16:07 +0200

    Task 0 done

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 ''' This module defines a function '''
 
 from asyncio import sleep
-from random import random, randint
+from random import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     ''' generates 10 random numbers '''
 
     for i in range(10):
 
         await sleep(1)
-        yield random() * randint(0, 10)
+        yield random() * 10
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' This module defines a function '''
+''' This defines a function '''
 
 
 async def async_comprehension():
