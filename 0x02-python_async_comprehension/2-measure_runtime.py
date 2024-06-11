c77507e46f89a3a67c1e3f76100a06cf762bba1d
Scion-Kin <dinturner17@gmail.com>
2024-06-11 12:37:11 +0200

    Task 2 done

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 ''' This module defines a function '''
+
+from asyncio import gather
+from time import time
+
+
+async def measure_runtime():
+    ''' measures the runtime of async_comprehension function '''
+
+    comp = __import__('1-async_comprehension').async_comprehension
+    start = time()
+    await gather(*(comp() for i in range(4)))
+    end = time()
+
+    return end - start
